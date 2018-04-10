bd76b683636884b3a4940fbf17bd60f56947b59e
oywy <415295962@qq.com>
2018-04-10 18:36:47 +0800

    这是新的分支
    Signed-off-by: oywy <415295962@qq.com>

--- src/main/java/com/oywy/SearchhouseApplication.java
+++ src/main/java/com/oywy/SearchhouseApplication.java
@@ -1,11 +1,11 @@
 package com.oywy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SearchhouseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SearchhouseApplication.class, args);
 	}
